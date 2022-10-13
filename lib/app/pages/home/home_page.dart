2b462bbb93e95b6cc75104c19f887e51e8e6d437
vinibressanini <vinimarcus41@gmail.com>
2022-10-13 15:40:46 -0300

    feat: Custom Dio sending access token with interceptor

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,4 +1,10 @@
+import 'package:dartweek_fwc/app/core/ui/styles/button_styles.dart';
+import 'package:dartweek_fwc/app/core/ui/styles/text_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+
+import '../../core/rest/custom_dio.dart';
+import '../../core/ui/widgets/button.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,7 +15,16 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: Container(),
+      body: Center(
+        child: Button(
+          label: 'Auth Test',
+          labelStyle: context.textStyles.textPrimaryFontBold,
+          style: context.buttonStyles.yellowButton,
+          onPressed: () {
+            context.get<CustomDio>().auth().get('/api/me');
+          },
+        ),
+      ),
     );
   }
 }
